5b95a003a30e62b6cfe57c3a77d50e57e3752321
Brad Ottley <bottley@rocketlawyer.co.uk>
2024-07-22 17:53:46 +0200

    Add payment method only

--- www/StripeUIPlugin.js
+++ www/StripeUIPlugin.js
@@ -2,5 +2,8 @@ var exec = require('cordova/exec');
 module.exports = {
 	presentPaymentSheet: function (paymentConfig, billingConfig, success, error) {
 		exec(success, error, "StripeUIPlugin", "presentPaymentSheet", [paymentConfig, billingConfig]);
+	},
+	confirmSetupIntent: function (config, success, error) {
+		exec(success, error, "StripeUIPlugin", "confirmSetupIntent", [config]);
 	}
 };
