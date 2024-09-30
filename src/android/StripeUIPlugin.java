a2f5f91be2f5d5f8a6241580c43e6242867e36c8
Brad Ottley <bottley@rocketlawyer.co.uk>
2024-09-30 16:24:06 +0200

    Android updates

--- src/android/StripeUIPlugin.java
+++ src/android/StripeUIPlugin.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -31,8 +32,7 @@ public class StripeUIPlugin extends CordovaPlugin {
                 callback = callbackContext;
                 JSONObject paymentConfig = !args.getString(0).equals("null") ? args.getJSONObject(0) : null;
                 JSONObject billingConfig = !args.getString(1).equals("null") ? args.getJSONObject(1) : null;
-                assert paymentConfig != null;
-                
+                assert paymentConfig != null;   
                 String publishableKey = paymentConfig.getString("publishableKey");
                 String companyName = paymentConfig.getString("companyName");
                 String paymentIntent = paymentConfig.optString("paymentIntentClientSecret", null);
@@ -41,7 +41,6 @@ public class StripeUIPlugin extends CordovaPlugin {
                 String ephemeralKey = paymentConfig.optString("customerEphemeralKeySecret", null);
                 String appleMerchantCountryCode = paymentConfig.optString("appleMerchantCountryCode", null);
                 boolean mobilePayEnabled = paymentConfig.optBoolean("mobilePayEnabled", false);
-
                 Intent intent = new Intent(cordova.getActivity().getApplicationContext(), CheckoutActivity.class);
                 intent.putExtra("publishableKey", publishableKey);
                 intent.putExtra("companyName", companyName);
@@ -62,7 +61,6 @@ public class StripeUIPlugin extends CordovaPlugin {
                     String billingLine2 = billingConfig.optString("billingLine2", null);
                     String billingPostalCode = billingConfig.optString("billingPostalCode", null);
                     String billingState = billingConfig.optString("billingState", null);
-
                     intent.putExtra("billingEmail", billingEmail);
                     intent.putExtra("billingName", billingName);
                     intent.putExtra("billingPhone", billingPhone);
@@ -73,7 +71,6 @@ public class StripeUIPlugin extends CordovaPlugin {
                     intent.putExtra("billingPostalCode", billingPostalCode);
                     intent.putExtra("billingState", billingState);
                 }
-
                 cordova.setActivityResultCallback(this);
                 cordova.getActivity().startActivityForResult(intent, 1);
             } catch (Throwable e) {
