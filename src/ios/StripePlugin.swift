34f3520a8535f999c982e588ada9d765be93f3c6
Brad Ottley <bottley@rocketlawyer.co.uk>
2025-01-28 13:14:47 +0200

    Apple Pay Config Logs

--- src/ios/StripePlugin.swift
+++ src/ios/StripePlugin.swift
@@ -1,4 +1,5 @@
 import UIKit
+import PassKit
 import StripePaymentSheet
 
 @objc(StripePlugin) class StripePlugin : CDVPlugin {
@@ -15,9 +16,11 @@ import StripePaymentSheet
         let customerId = (paymentConfig["customerId"] ?? "") as? String ?? ""
         let customerEphemeralKeySecret = (paymentConfig["customerEphemeralKeySecret"] ?? "") as? String ?? ""
         let appleMerchantId = (paymentConfig["appleMerchantId"] ?? "") as? String ?? ""
-        let appleMerchantCountryCode = (paymentConfig["appleMerchantCountryCode"] ?? "") as? String ?? ""
+        let merchantCountryCode = (paymentConfig["merchantCountryCode"] ?? "") as? String ?? ""
         let mobilePayEnabled = (paymentConfig["mobilePayEnabled"] ?? false) as? Bool ?? false
         let returnURL = (paymentConfig["returnURL"] ?? "") as? String ?? ""
+        let primaryButtonLabel = (paymentConfig["primaryButtonLabel"] ?? "") as? String ?? ""
+        let applePaymentSummaryItems = (paymentConfig["applePaymentSummaryItems"] ?? []) as? [[String: Any]] ?? []
 
         // Customer Billing Details
         let billingConfig = (command.argument(at: 1) ?? [String: Any]()) as? [String: Any] ?? [String: Any]()
@@ -26,6 +29,7 @@ import StripePaymentSheet
         
         STPAPIClient.shared.publishableKey = publishableKey
 
+        var useMobilePay = false
         var configuration = PaymentSheet.Configuration()
         
         // Currently dark mode not supported
@@ -40,18 +44,88 @@ import StripePaymentSheet
             configuration.merchantDisplayName = companyName
         }
         if customerId != "" && customerEphemeralKeySecret != "" {
-             configuration.customer = .init(id: customerId, ephemeralKeySecret: customerEphemeralKeySecret)
+            configuration.customer = .init(id: customerId, ephemeralKeySecret: customerEphemeralKeySecret)
         }
         
-        configuration.applePay = .init(merchantId: appleMerchantId, merchantCountryCode: appleMerchantCountryCode)
+        if mobilePayEnabled {
+            print("mobilePayEnabled is enabled")
+            let formatter = NumberFormatter()
+            formatter.generatesDecimalNumbers = true
+
+            var paymentSummaryItems = [PKPaymentSummaryItem]()
+            for applePaymentSummaryItem in applePaymentSummaryItems {
+                let itemAmount = formatter.number(from: applePaymentSummaryItem["amount"] as! String) as? NSDecimalNumber ?? 0
+
+                print("itemAmount: \(itemAmount)")
+
+                print("applePaymentSummaryItem: \(applePaymentSummaryItem)")
+
+                if applePaymentSummaryItem["isRecurring"] as! Bool { 
+
+                    print("Is Recurring payment")
+                    let recurringStartDateValue = (applePaymentSummaryItem["recurringStartDateValue"] as? TimeInterval) ?? 0
+
+                    print("recurringStartDateValue: \(recurringStartDateValue)")
+
+                    if #available(iOS 15.0, *) {
+
+                        print("iOS 15.0 + : true")
+
+                        let recurringItem = PKRecurringPaymentSummaryItem(label: applePaymentSummaryItem["label"] as! String, amount: itemAmount)
+
+                        print("recurringItem: \(recurringItem)")
+
+                        recurringItem.intervalCount = 1
+                        recurringItem.intervalUnit = (applePaymentSummaryItem["recurringIntervalUnit"] as! String == "MONTHLY") ? .month : .year
+                        if recurringStartDateValue > 0 {
+                            recurringItem.startDate = Date(timeIntervalSince1970: recurringStartDateValue / 1000)
+                        }
+                        paymentSummaryItems.append(recurringItem)
+
+                        print("paymentSummaryItems: \(paymentSummaryItems)")
+                    } else {
+                         print("iOS 15.0 + : false")
+                        let dateFormatter = DateFormatter()
+                        dateFormatter.dateStyle = .medium
+                        let recurringStartDate = Date(timeIntervalSince1970: recurringStartDateValue / 1000)
+
+                        let labelWithDate = (applePaymentSummaryItem["label"] as! String) + (recurringStartDateValue > 0 ? (" From " + dateFormatter.string(from: recurringStartDate)) : "")
+                        let summaryItem = PKPaymentSummaryItem(label:labelWithDate, amount: itemAmount)
+                        paymentSummaryItems.append(summaryItem)
+                    }
+                } else {
+
+                    print("NOT Recurring payment")
+                    let summaryItem = PKPaymentSummaryItem(label: applePaymentSummaryItem["label"] as! String, amount: itemAmount)
+                    paymentSummaryItems.append(summaryItem)
+                }
+            }
+
+            let handlers = PaymentSheet.ApplePayConfiguration.Handlers(paymentRequestHandler: nil, authorizationResultHandler: {
+                paymentAuthorizationResult, completion in
+                if paymentAuthorizationResult.status == .success {
+                    useMobilePay = true                    
+                } else {
+                    for error in paymentAuthorizationResult.errors {
+                        print("authorizationResultHandler error: \(error.localizedDescription)\n")
+                    }
+                }
+                completion(paymentAuthorizationResult)
+            })
+
+            configuration.applePay = .init(merchantId: appleMerchantId, merchantCountryCode: merchantCountryCode, buttonType: PKPaymentButtonType.inStore, paymentSummaryItems: paymentSummaryItems, customHandlers: handlers)
+        }
 
         configuration.allowsDelayedPaymentMethods = true
         configuration.defaultBillingDetails.email = billingEmail
         configuration.defaultBillingDetails.name = billingName
-        configuration.defaultBillingDetails.address.country = "US"
+        configuration.defaultBillingDetails.address.country = merchantCountryCode
         
         configuration.billingDetailsCollectionConfiguration.name = .always
         configuration.billingDetailsCollectionConfiguration.address = .automatic
+        if primaryButtonLabel != "" {
+          configuration.primaryButtonLabel = primaryButtonLabel
+        }
 
         // Payment Sheet UI
         var appearance = PaymentSheet.Appearance()
@@ -71,7 +145,7 @@ import StripePaymentSheet
         paymentSheet?.present(from: self.viewController) { paymentResult in
             switch paymentResult {
                 case .completed:
-                    let message = ["code": "0", "message": "PAYMENT_COMPLETED"] as [AnyHashable : Any]
+                    let message = ["code": "0", "message": "PAYMENT_COMPLETED", "useMobilePay": useMobilePay] as [AnyHashable : Any]
                     let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: message)
                     self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
                 case .canceled:
@@ -85,6 +159,7 @@ import StripePaymentSheet
             }
         }
     }
+
     @objc(retrieveSetupIntent:)
     func retrieveSetupIntent(command: CDVInvokedUrlCommand){
         let clientSecret = (command.argument(at: 0) ?? "") as? String ?? ""
